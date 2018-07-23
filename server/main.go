49aff7f8db28498353dd43d3328979c3cea9e931
eeyoo <feilinwu2036@gmail.com>
2018-07-23 13:51:28 +0800

    web server

---
+++ server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func firstPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello, this is index page</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", firstPage)
+	http.ListenAndServe(":8000", nil)
+}
