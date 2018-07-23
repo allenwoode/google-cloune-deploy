5d80b077b8aaeb0b3a046c498bbdb5b1b2ac733a
eeyoo <feilinwu2036@gmail.com>
2018-07-23 15:23:55 +0800

    deploy shell

--- deployserver/deploy.sh
+++ deployserver/deploy.sh
@@ -1,7 +1,11 @@
 #! /bin/sh
 
 kill -9 $(pgrep server)
-git pull https://github.com/allenwoode/google-cloune-deploy.git
+
 cd ~/google-cloune-deploy
+
+git pull https://github.com/allenwoode/google-cloune-deploy.git
+
 ./server/server &
+
 echo "relanch server"
